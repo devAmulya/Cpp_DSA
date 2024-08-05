bdb42fe763f147f90411203c0e316476759a6e5f
devAmulya <amulyagupta57121@gmail.com>
2024-08-05 21:04:50 +0530

    practise

--- V8/mnc_ques.cpp
+++ V8/mnc_ques.cpp
@@ -9,6 +9,7 @@ int main(){
     for(int i = 0; i < n; i++){
         cin>>arr[i];
     }
+    /* First repeating element with smallest i
     long long int N = 100000;
     int idx[N];
     for(int i = 0; i  < N; i++){
@@ -24,8 +25,54 @@ int main(){
         }
     }
 
-    cout<<min_idx;
+    cout<<min_idx;*/
 
+    /*subarray = S unsorted constraints : 1 <= N <= 10^5
+    int s = 12;
+    int i = 0, j = 0, sum = 0, start = -1, end = -1;
+    while(j<n && sum + arr[j] <= s){
+        sum += arr[j];
+        j++;
+    }
+    if(sum == s){
+        cout<<i<<" "<<j-1;
+        return 0;
+    }
+    while(j<n){
+        sum += arr[j];
+        while(sum>s){
+            sum -= arr[i];
+            i++;
+        }
+        if(sum == s){
+            start = i;
+            end = j;
+            break;
+        }
+        j++;
+        
+    }
+    cout<<start<<" "<<end;*/
+
+    // Smallest possitive integer missing
+    long long int N = 1000000;
+    bool idx[N];
+    for(int i = 0; i< N; i++){
+        idx[i] = false;
+    }
+    for(int i = 0; i < n; i++){
+        if(arr[i] >= 0){
+            idx[arr[i]] = true;
+        }
+    }
+    int ans = -1;
+    for(int i = 0; i < N; i++){
+        if(idx[i] == false){
+            ans = i;
+            break;
+        }
+    }
+    cout<<ans;
     return 0;
     
 
