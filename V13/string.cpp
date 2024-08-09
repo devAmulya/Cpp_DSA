65de5912116715eb736614217b8a9d09cfc76a7a
devAmulya <amulyagupta57121@gmail.com>
2024-08-10 00:01:16 +0530

    String

---
+++ V13/string.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+
+    string str;
+
+    /*cin >> str;
+    cout<<str<<endl;
+
+    string str1(5,'n');
+    cout<<str1<<endl;
+
+    string str2 = "Apni kaksha";
+    cout<<str2<<endl;*/
+
+    getline(cin,str);
+    cout<<str;
+
+    return 0;
+}
