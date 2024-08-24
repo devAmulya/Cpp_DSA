9b63e1e3d8f5364ec32aa9268d1a5acbb0a4940d
devAmulya <amulyagupta57121@gmail.com>
2024-08-24 18:23:52 +0530

    Bit Manipulation

--- V14/BM1.cpp
+++ V14/BM1.cpp
@@ -2,14 +2,32 @@
 using namespace std;
 
 int getBit(int n, int pos){
-    return n&(1<<pos);
+    return (n&(1<<pos)) != 0;
+}
+
+int setBit(int n, int pos){
+    return(n|(1<<pos));
+}
+
+int clearBit(int n, int pos){
+    return(n&(~(1<<pos)));
+}
+
+int updateBit(int n, int pos, int up){
+    if(up){
+        return setBit(n,pos);
+    }else{
+        return clearBit(n,pos);
+    }
 }
 
 int main(){
 
     int n = 5;
-    int b = {getBit(5,2) == 0 ? 0:1};
-    cout << b;
+    /*cout << getBit(n,2)<<endl; //Give Bit value at pos 
+    cout << setBit(n,1)<<endl; //SetBit to 1
+    cout << clearBit(n,2)<<endl; //Clear bit - set bit = 0*/
+    cout<< updateBit(n,0,1);
     
 
     return 0;
