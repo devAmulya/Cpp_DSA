256f4791d9e05263efe4648f885be75d589c58c2
devAmulya <amulyagupta57121@gmail.com>
2024-09-22 17:35:39 +0530

    intro to recursion

--- V16/recursion_intro.cpp
+++ V16/recursion_intro.cpp
@@ -1,8 +1,42 @@
 #include<iostream>
 using namespace std;
 
-int main(){
+int sum(int n){
+    if(n==0){
+        return 0;
+    }
+    return (n+sum(n-1));
+}
 
-    
+int power(int n, int y){
+    if(y == 0){
+        return 1;
+    }
+    return n*power(n,y-1);
+}
+
+int factorial(int n){
+    if(n == 0){
+        return 1;
+    }
+    return n*factorial(n-1);
+}
+
+int fibonacci(int n){
+    if(n == 0){
+        return 0;
+    }
+    if(n == 1){
+        return 1;
+    }
+    return fibonacci(n-1) + fibonacci(n-2);
+}
+
+int main(){
+    cout<<"Sum = "<<sum(5)<<endl;
+    cout<<"Power = "<<power(2,5)<<endl;    
+    cout<<"Factorial = "<<factorial(6)<<endl;
+    cout<<"Fibonacci = "<<fibonacci(10)<<endl;
+
     return 0;
 }
